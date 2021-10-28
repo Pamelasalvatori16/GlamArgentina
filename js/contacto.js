55e9c7b7d92523e87c572f6f061499ec0dfa47bd
PamelaSalvatori16 <licpamelasalvatori@gmail.com>
2021-10-28 19:47:18 -0300

    jsvalidation

--- js/contacto.js
+++ js/contacto.js
@@ -1,36 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("form").addEventListener('submit', validarFormulario); 
-  });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    var nombreApellido = document.getElementById('nombreApellido').value;
-    if(nombreApellido.length == 0) {
-      alert('Por favor, escriba su nombre y apellido');
-      return;
-    }
+function validar() {
+  let nombreApellido, email, tel, tel2;
+  nombreApellido = document.getElementById("nombreApellido").value;
+  email = document.getElementById("email").value;
+  cArea= document.getElementById("cArea").value;
+  tel2 = document.getElementById("tel2").value;
 
-    var tel = document.getElementById('tel').value;
-    if (tel.length == 0) {
-      alert('Por favor, escriba el código de área');
-      return;
-    }
+  expresion = /\w+@\w+\.+[a-z]/;
+  expresion1= /^[A-Z ]+$/i;
 
-    var tel2 = document.getElementById('tel2').value;
-    if (tel2.length == 0) {
-      alert('Por favor, escriba su número de teléfono');
-      return;
-    }
-
-    var expresion = /^[a-z][\w.-]+@\w[\w.-]+\.[\w.-]*[a-z][a-z]$/i;
-    var email = document.getElementById('email').value;
-    if (email.length == 0 || !expresion.test(email)) {
-      alert('Por favor, revise el campo email');
-      return;
-    }
-   
-    this.submit();
-    alert("El formuario ha ido enviado con éxito");
+  if (nombreApellido ==="" || cArea ==="" || tel2 ==="" || email ==="") {
+    alert("Debe completar todos los campos obligatorios");
+    return false;
+  }
+  else if (! expresion1.test(nombreApellido)) {
+    alert("El campo nombre y apellido no es válido"); 
+    return false;
+  }
+  else if (cArea.length > 5) {
+  alert("El código de área es demasiado largo"); 
+  return false;
+  }
+  else if (isNaN(cArea)){
+  alert("El código de área no es un número");
+  return false;
+  }
+  else if (tel2.length > 9) {
+  alert("El teléfono ingresado es demasiado largo"); 
+  return false;
+  }
+  else if (isNaN(tel2)){
+  alert("El teléfono ingresado no es un número");
+  return false;
+  }
+  else if (! expresion.test(email)) {
+  alert("El correo no es válido");
+  return false;
+  }
+  else {
+  alert("El formulario fue enviado con éxito")
+  }
 }
-
-
